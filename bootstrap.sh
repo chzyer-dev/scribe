aa3cf7b67a9efda7c0bb6996e769adc2d6fc04b1
chzyer <me@chenye.org>
2015-03-06 15:49:56 +0800

    fix automake bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@
 
 # To be safe include -I flag
 autoreconf --force --verbose --install
-./configure --config-cache $*
+./configure --with-boost-system=boost_system --with-boost-filesystem=boost_filesystem --config-cache $*
+#./configure --config-cache $*
